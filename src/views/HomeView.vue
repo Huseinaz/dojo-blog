d9b60dc6ff8ea61ebcfc9e2b25ceaae29c6aeb8f
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-11-01 12:20:57 +0200

    Refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>My name is {{ name }} I'm {{ age }} years old.</h2>
+    <p ref="p">My name is {{ name }} I'm {{ age }} years old.</p>
     <button @click="handleClick">Click Me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'HomeView',
   setup() {
-    console.log('setup');
+    const p = ref(null)
     
     let name = 'Hussein'
     let age = 24
 
     const handleClick = () => {
-      console.log('You clicked me.');
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'Hello again.'
     }
     
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   },
 }
 </script>
