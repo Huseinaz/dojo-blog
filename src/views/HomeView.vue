652cdf250c47ee2906943bd5ac98d560f5db3777
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-11-02 12:13:36 +0200

    Lifecycle hooks in setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" />
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">Toggle posts</button>
+    <button @click="posts.pop()">Delete a post</button>
   </div>
 </template>
 
@@ -17,8 +19,9 @@ export default {
       { title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptate asperiores delectus quae nam natus cupiditate rem quis facere necessitatibus quidem, laudantium molestiae, dolores at, sapiente aspernatur! Doloribus, neque et!', id: 1 },
       { title: 'Top 5 CSS tips', body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum omnis quod doloribus, animi molestiae voluptates recusandae, quidem eligendi odio expedita eaque. Fugit dolorum quibusdam earum illo, ipsum placeat eveniet officiis.', id: 2 }
     ])
+    const showPosts = ref(true)
     
-    return { posts }
+    return { posts, showPosts }
   },
 }
 </script>
