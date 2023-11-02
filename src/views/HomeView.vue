29542d00771a1300168f056cb68a940d54210a52
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-11-02 12:54:58 +0200

    Fetching data in setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">Toggle posts</button>
-    <button @click="posts.pop()">Delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>
+      Loading...
+    </div>
   </div>
 </template>
 
@@ -15,13 +19,26 @@ export default {
   name: 'HomeView',
   components: { PostList },
   setup() {
-    const posts = ref([
-      { title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptate asperiores delectus quae nam natus cupiditate rem quis facere necessitatibus quidem, laudantium molestiae, dolores at, sapiente aspernatur! Doloribus, neque et!', id: 1 },
-      { title: 'Top 5 CSS tips', body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum omnis quod doloribus, animi molestiae voluptates recusandae, quidem eligendi odio expedita eaque. Fugit dolorum quibusdam earum illo, ipsum placeat eveniet officiis.', id: 2 }
-    ])
-    const showPosts = ref(true)
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok){
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+      }
+      catch(err) {
+        error.value = err.message   //update error=ref(null) to throw error msg
+        console.log(error.value);
+      }
+    }
+
+    load()
     
-    return { posts, showPosts }
+    return { posts, error }
   },
 }
 </script>
